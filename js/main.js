28b6b2e754972c048a59f2fa31b3287185c01c3f
Aniket Handa <atneik@gmail.com>
2015-09-02 02:27:42 -0700

    Added device class
    Updates data in device class if new data point is found.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var KiloWalk = KiloWalk || {};
 /////////////////////////////////////////
-// Fetch Data
+// Fetch Devices
 /////////////////////////////////////////
 KiloWalk.map = {
   sortObj: {  "published": -1 },
@@ -46,18 +46,71 @@ KiloWalk.map = {
         L.tileLayer.provider('Stamen.Toner').addTo(map);
         first = false;
       }
-      var circle = L.circle(devices[device].lat_long, 300, {
-          color: 'red',
-          fillColor: '#f03',
-          fillOpacity: 0.5,
-          id: 'marker'
-      }).addTo(map);
+      var device = new KiloWalk.Device(device, devices[device].lat_long);
+      device.plotOnMap(map);
+      device.startUpdate();
     }
   },
   render: function(){
     this.getData(this.plot);
   }
 }
+/////////////////////////////////////////
+// Device class
+/////////////////////////////////////////
+KiloWalk.Device = function(deviceID, deviceLat_long) {
+  this.deviceID = deviceID;
+  this.lat_long = deviceLat_long;
+  this.circle = null;
+  this.update = false;
+  this.data = [];
+
+  this.refreshDevice = function(){
+    console.log(this.data);
+  }
+  this.addLatestDataPoint = function(){
+    if(this.update){
+      var sortObj = { "published": -1 };
+      var queryObj = { "deviceID": this.deviceID};
+      var url = "https://api.mongolab.com/api/1/databases/heroku_23wdtw34/collections/sensordata" + 
+          "?s=" + encodeURIComponent(JSON.stringify(sortObj)) + 
+          "&q=" + encodeURIComponent(JSON.stringify(queryObj)) +
+          "&l=1&apiKey=" + _API_KEY;
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function() {
+          if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+              var data = JSON.parse(xmlhttp.responseText);
+              if(this.data.length === 0 || this.data[this.data.length - 1]["_id"]["$oid"] != data[0]["_id"]["$oid"]){
+                this.data.push(data[0]);
+                this.refreshDevice();
+              } else {
+                console.log(this.deviceID,"-no new data");
+              }
+              setTimeout(function(){ this.addLatestDataPoint() }.bind(this), 3000);
+          }
+      }.bind(this);
+      xmlhttp.open("GET", url, true);
+      xmlhttp.send();
+    }
+  }
+}
+KiloWalk.Device.prototype.plotOnMap = function(map){
+  this.circle = L.circle(this.lat_long, 300, {
+          color: 'red',
+          fillColor: '#f03',
+          fillOpacity: 0.5,
+          id: 'marker',
+          deviceID: this.deviceID
+      }).addTo(map);
+}
+KiloWalk.Device.prototype.startUpdate = function(){
+  this.update = true;
+  this.addLatestDataPoint();
+}
+KiloWalk.Device.prototype.stopUpdate = function(){
+  this.update = false;
+}
+
 /////////////////////////////////////////
 // MAIN
 /////////////////////////////////////////
